b38fdc4f0d17db3dc2ac1bb9e95a164edcea366e
Manish Shaw <monishofficial8@gmail.com>
2023-03-28 18:05:30 +0530

    First Commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {AppBar, Box, IconButton, Toolbar} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import { selectToggler, toggleDrawer } from '../features/toggleSlice.js';
+import { useSelector, useDispatch } from "react-redux";
+
+import { Link } from 'react-router-dom';
+
+const handleClick = (isShown, dispatch) => {
+  dispatch(toggleDrawer(!isShown))
+}
+
+const NavBar = () => {
+  const isShown = useSelector(selectToggler)
+  const dispatch = useDispatch();
+  return (
+    <AppBar sx={{ position: 'relative' }}>
+      <Toolbar>
+        <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={() => handleClick(isShown, dispatch)}
+            sx={{ display: { sm: 'none' } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Toolbar sx={{ px: '1rem', width: '100%' }}>
+          <Link to="/" style={{ color: 'inherit', padding: '1rem', textDecoration:"none" }}>Home</Link>
+          <Link to="/history" style={{ color: 'inherit', padding: '1rem', textDecoration:"none"  }}>History</Link>
+        </Toolbar>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default NavBar
